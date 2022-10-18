66bda2fdf5fc80d6e3a21aed271eed2b7b95d4bc
thedamilola <tomosoridamilola48@gmail.com>
2022-10-18 13:23:52 -0700

    Update Git Identity

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -18,10 +18,10 @@ int main(void)
 				putchar((i / 10) + 48);
 				putchar((i % 10) + 48);
 				putchar((j / 10) + 48);
+				putchar((j % 10) + 48);
 				if (i != 98 || j != 99)
 				{
-					putchar(',');
-					putchar(' ');
+					putchar(', ');
 				}
 			}
 		}
