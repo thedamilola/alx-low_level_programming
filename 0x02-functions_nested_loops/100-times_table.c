04b8d9ba28a853099b8d909c86c053b73eb58ae8
thedamilola <tomosoridamilola48@gmail.com>
2022-10-19 03:17:19 +0100

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -23,13 +23,11 @@ void print_times_table(int n)
 				_putchar(' ');
 
 				prod = num * mult;
-
 				
 				if (prod <= 99)
 					_putchar(' ');
 				if (prod <= 9)
 					_putchar(' ');
-
 				
 				if (prod >= 100)
 				{
