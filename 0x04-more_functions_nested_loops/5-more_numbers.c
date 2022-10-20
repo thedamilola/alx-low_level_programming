b67090df65c8ba3c23051f68a3e03143b06b55e6
thedamilola <tomosoridamilola48@gmail.com>
2022-10-21 00:48:59 +0100

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,10 +12,8 @@ void more_numbers(void)
 	{
 		for (num = 0; num <= 14; num++)
 		{
-			if (num > 0)
-			{
-				_putchar((num / 10) + '0');
-			}
+			if (num >= 10)
+				_putchar('1');
 			_putchar((num % 10) + '0');
 		}
 
