047fd794ebdfc749f53fc7ba814d79212c9f47fe
thedamilola <tomosoridamilola48@gmail.com>
2022-11-15 01:34:50 +0100

    3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,7 @@
-#ifndef FUNCTION_LIKE_MACRO_H
-#define FUNCTION_LIKE_MACRO_H
+#ifndef 3_FUNCTION_LIKE_MACRO_H
+#define 3_FUNCTION_LIKE_MACRO_H
+
 #define ABS(x) ((x) < 0 ? (-(x)) : (x))
 
-#endif
+#endif /* 3_FUNCTION_LIKE_MACRO_H */
 ~
